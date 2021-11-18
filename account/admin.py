bb57ab88ed59fd49a59f34918c226b514b663aa9
sooyeon-kr <sooyeon7027@naver.com>
2021-11-19 00:17:51 +0900

    Add custom user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models # 👈 해당 model이 존재하는 파일을 import
 
-# Register your models here.
+# admin.site.register(User)
+@admin.register(models.User) # 👈 데코레이터로 등록
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- account/models.py
+++ account/models.py
@@ -1,3 +1,22 @@
-from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 # Create your models here.
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    """Custom User Model"""
+    
+    TYPE_PROTECTOR = "protector"
+    TYPE_PROTEGE = "protege"
+    TYPE_OTHER = "other"
+    TYPE_CHOICES = (
+        (TYPE_PROTECTOR, "Protector"),
+        (TYPE_PROTEGE, "Protege"),
+        (TYPE_OTHER, "Other"),
+    )
+
+    type = models.CharField(
+        choices=TYPE_CHOICES, max_length=10, null=True, blank=True
+    )
+
