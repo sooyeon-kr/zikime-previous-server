137977f10e42b4931b8bce624ad1864dbb884353
sooyeon-kr <sooyeon7027@naver.com>
2021-11-18 23:02:43 +0900

    Add Custom User model
    -

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from . import models # 👈 해당 model이 존재하는 파일을 import
+@admin.register(models.User) # 👈 데코레이터로 등록
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
